836a7eda54f4fb94dbc103809ff1eb99b6abb176
rafapitzer <rafa_thadeupitzer@hotmail.com>
2021-11-02 16:54:09 -0300

    project creation

---
+++ src/main/java/Chess/ChessMatch.java
@@ -0,0 +1,22 @@
+package Chess;
+
+import boardgame.Board;
+
+public class ChessMatch {
+
+    private Board board;
+
+    public ChessMatch() {
+        board = new Board(8,8);
+    }
+
+    public ChessPiece[][] getPieces() {
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+        for(int x = 0; x < board.getRows(); x++) {
+            for(int y = 0; y < board.getColumns(); y++){
+                mat[x][y] = (ChessPiece) board.piece(x,y);
+            }
+        }
+        return mat;
+    }
+}
---
+++ src/main/java/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import Chess.ChessMatch;
+
+public class Program {
+
+    public static void main(String args[]) {
+
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
+    }
+
+}
